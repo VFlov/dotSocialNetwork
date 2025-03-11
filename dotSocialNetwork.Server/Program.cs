85d0418f525508302b6c77128b71dd22ceb12c0a
VFlov <129610413+VFlov@users.noreply.github.com>
2025-03-11 10:38:32 +0500

    update

--- dotSocialNetwork.Server/Program.cs
+++ dotSocialNetwork.Server/Program.cs
@@ -35,6 +35,7 @@ builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
             }
         };
     });
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddHttpContextAccessor();
